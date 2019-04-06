054de47b2c1fc0842c1c585ddb988c2cf48c87ce
Vegar Andreas Bergum <vegarab@stud.ntnu.no>
2019-04-06 22:07:30 +0200

    Functional model with 2D one-hot encoded inputs

--- train.py
+++ train.py
@@ -1,6 +1,7 @@
 from keras.models import Model
 from keras.layers import Input, LSTM, Dense, Embedding
 import numpy as np
+import os
 
 from data import get_utterance_pairs, get_word_map
 
@@ -21,18 +22,21 @@ max_decoder_seq_length = max(len(utterance) for utterance in target_utterances)
 num_encoder_tokens = len(input_token_to_num)
 num_decoder_tokens = len(target_token_to_num)
 
-encoder_input_data = np.zeros((len(input_utterances), max_encoder_seq_length, num_encoder_tokens), dtype='float32')
-decoder_input_data = np.zeros((len(target_utterances), max_decoder_seq_length, num_decoder_tokens), dtype='float32')
+encoder_input_data = np.zeros((len(input_utterances), max_encoder_seq_length), dtype='float32')
+decoder_input_data = np.zeros((len(target_utterances), max_decoder_seq_length), dtype='float32')
 decoder_target_data = np.zeros((len(target_utterances), max_decoder_seq_length, num_decoder_tokens), dtype='float32')
 
+
 for i, (input_utterance, target_utterance) in enumerate(zip(input_utterances, target_utterances)):
+    # Splice to remove <u> and </u>
+    target_utterance = target_utterance[1:-1]
     for j, token in enumerate(input_utterance):
         # One-hot encoding for which tokens appear in the encoder input data.
-        encoder_input_data[i, j, input_token_to_num[token]] = 1
+        encoder_input_data[i][j] = input_token_to_num[token]
 
     for k, token in enumerate(target_utterance):
         # One-hot encoding for decoder input data.
-        decoder_input_data[i, k, target_token_to_num[token]] = 1
+        decoder_input_data[i][j] = input_token_to_num[token]
 
         if k > 0:
             # One-hot encoding for decoder targets. This is the same
@@ -40,7 +44,7 @@ for i, (input_utterance, target_utterance) in enumerate(zip(input_utterances, ta
             # adding tokens from is shifted to the right by one so that
             # the special token marking the start of an utterance is
             # no longer included.
-            decoder_target_data[i, k - 1, target_token_to_num[token]] = 1
+            decoder_target_data[i, k-1, target_token_to_num[token]] = 1
 
 LATENT_DIM = 256
 BATCN_SIZE = 64
@@ -57,13 +61,18 @@ x = LSTM(LATENT_DIM, return_sequences=True)(x, initial_state=encoder_states)
 decoder_outputs = Dense(num_decoder_tokens, activation='softmax')(x)
 
 model = Model([encoder_inputs, decoder_inputs], decoder_outputs)
-
 model.compile(optimizer='rmsprop', loss='categorical_crossentropy')
 
-# TODO: "Note that `decoder_target_data` needs to be one-hot encoded,
-# rather than sequences of integers like `decoder_input_data`!"
-model.fit([encoder_input_data, decoder_input_data], decoder_target_data,
-          batch_size=BATCN_SIZE,
-          epochs=NUM_EPOCHS,
-          validation_split=0.2)
+MODEL_PATH = 'models/model.h5'
+if not os.path.isfile('models/model.h5'):
+    # TODO: "Note that `decoder_target_data` needs to be one-hot encoded,
+    # rather than sequences of integers like `decoder_input_data`!"
+    model.fit([encoder_input_data, decoder_input_data], decoder_target_data,
+              batch_size=BATCN_SIZE,
+              epochs=NUM_EPOCHS,
+              validation_split=0.2)
+    model.save_weights(MODEL_PATH)
+else: 
+    model.load_weights(MODEL_PATH)
 
+model.summary()
