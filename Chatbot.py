9d8c88caf63a17782a88b1d309576e2d60bb7eba
Vegar Andreas Bergum <vegarab@stud.ntnu.no>
2019-04-09 13:40:51 +0200

    Improved model structure and added separate encoder- and decoder-models for actual prediction

--- Chatbot.py
+++ Chatbot.py
@@ -58,23 +58,36 @@ class Chatbot():
 
     def __build_model(self):
         encoder_inputs = Input(shape=(None,))
-        x = Embedding(self.num_encoder_tokens, LATENT_DIM)(encoder_inputs)
-        x, state_h, state_c = LSTM(LATENT_DIM, return_state=True)(x)
+        encoder_embedding = Embedding(input_dim=self.num_encoder_tokens, output_dim=LATENT_DIM)(encoder_inputs)
+        encoder_LSTM = LSTM(LATENT_DIM, return_sequences=True, return_state=True)
+        encoder_outputs, state_h, state_c = encoder_LSTM(encoder_embedding)
         encoder_states = [state_h, state_c]
 
         decoder_inputs = Input(shape=(None,))
-        x = Embedding(self.num_decoder_tokens, LATENT_DIM)(decoder_inputs)
-        x = LSTM(LATENT_DIM, return_sequences=True)(x, initial_state=encoder_states)
-        decoder_outputs = Dense(self.num_decoder_tokens, activation='softmax')(x)
+        decoder_embedding = Embedding(input_dim=self.num_decoder_tokens, output_dim=LATENT_DIM)(decoder_inputs)
+        decoder_LSTM = LSTM(LATENT_DIM, return_sequences=True, return_state=True)
+        decoder_outputs,_,_ = decoder_LSTM(decoder_embedding, initial_state=encoder_states)
+        decoder_dense = Dense(self.num_decoder_tokens, activation='softmax')
+        decoder_outputs = decoder_dense(decoder_outputs)
 
         self.model = Model([encoder_inputs, decoder_inputs], decoder_outputs)
         self.model.compile(optimizer='rmsprop', loss='categorical_crossentropy')
+
+        # Used for encoding incoming sentences when predicting output
+        self.encoder_model = Model(encoder_inputs, encoder_states)
+
+        decoder_state_inputs = [Input(shape=(LATENT_DIM,)), Input(shape=(LATENT_DIM,))]
+        decoder_outputs, state_h, state_c = decoder_LSTM(decoder_embedding, initial_state=decoder_state_inputs)
+        decoder_states = [state_h, state_c]
+        decoder_outputs = decoder_dense(decoder_outputs)
+        # Used for decoding incoming sentences when predicting output
+        self.decoder_model = Model([decoder_inputs] + decoder_state_inputs, [decoder_outputs] + decoder_states)
 
     def __train(self):
         if not os.path.isfile(MODEL_PATH):
             # TODO: "Note that `decoder_target_data` needs to be one-hot encoded,
             # rather than sequences of integers like `decoder_input_data`!"
-            self.model.fit([encoder_input_data, decoder_input_data], decoder_target_data,
+            self.model.fit([self.encoder_input_data, self.decoder_input_data], self.decoder_target_data,
                       batch_size=BATCN_SIZE,
                       epochs=NUM_EPOCHS,
                       validation_split=0.2)
